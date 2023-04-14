ed0e7b46cfcd69537fa458b1018b2c6c8facf596
unknown <sharmagaurav01012001@gmai.com>
2023-04-14 15:23:20 +0530

    initial commit

---
+++ jewe/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Navbar from 'Navbar';
+// import Navbar from 'Body';
+
+ App = () => {
+  return (
+    <>
+    <div>App</div>
+    <Navbar/>
+    <Body/>
+    </>
+  );
+};
+ export default App;
